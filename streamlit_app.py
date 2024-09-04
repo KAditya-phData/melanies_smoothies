ac81bf89554e4777312e9216ced042f9e8f74839
KAditya-phData <kaditya@phdata.io>
2024-09-04 15:09:39 +0530

    Update streamlit_app.py
    updated
    
    # st.text(fruityvice_response.json())
    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from snowflake.snowpark.functions import col
 # New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -45,3 +44,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
